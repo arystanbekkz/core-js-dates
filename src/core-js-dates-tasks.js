a7b7ef605a79912f451f50110fb36642b8e4f3ca
Arystanbek Baigeldinov <arystanbek.baigeldinov@gmail.com>
2025-12-17 17:35:56 +0500

    feat

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -201,8 +201,11 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const d = new Date(date);
+  d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
+  const yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+  return Math.ceil(((d - yearStart) / 86400000 + 1) / 7);
 }
 
 /**
